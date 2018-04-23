e4e324cf9c55b37c1eef27019742adeddb79e570
AnnBenavides <andrea.benavidesj@gmail.com>
2018-04-23 16:35:59 -0300

    dealing busy waiting with nCondition

--- subasta.c
+++ subasta.c
@@ -10,6 +10,7 @@ typedef struct oferente{
 	Estado e;
 	double precio;
 	nCondition c;
+	int ready;
 } *Oferente;
 
 typedef struct subasta {
@@ -20,6 +21,9 @@ typedef struct subasta {
   double min;
   int minIndex;
 } *Subasta;
+
+// Programe aca las funciones nuevaSubasta, ofrecer y adjudicar, mas
+// otras funciones que necesite.
 
 int compararPrecios(Subasta s,Oferente o){
 	//tomando el precio minimo ofertado en la subasta (s->min)
@@ -34,7 +38,6 @@ int compararPrecios(Subasta s,Oferente o){
 	else 
 		return -1;
 }
-
 void menorPostor(Subasta s){
 	// se calcula el menor precio (s->min) entre los oferentes(s->o[:]) de la subasta
 	// y se guarda el minimo entre estos (s->minIndex)
@@ -48,7 +51,8 @@ void menorPostor(Subasta s){
 int swap(Subasta s, Oferente o){
 	//al ofrecer, como el nuevo oferente tiene una mejor apuesta; entra a la subasta:
 	// se saca el oferente desplazado (s->minIndex) y se le avisa que esta afuera (s->o[minIndex].e=afuera)
-	s->o[minIndex]->e = afuera;
+	s->o[minIndex]->e=afuera;
+	s->o[minIndex]->ready=TRUE;
 	nSignalCondition(s->o[minIndex]->c);
 	// en su lugar se pone el nuevo oferente(s->o[minIndex]=o) y se le avisa que esta adentro(o->e=dentro)
 	o->e=dentro;
@@ -56,25 +60,6 @@ int swap(Subasta s, Oferente o){
 	// se recalcula el menor precio entre los oferentes de la subasta (menorPostor(s))
 	menorPostor(s);
 }
-
-Oferente initOferta(Subasta s, double precio){
-	Oferente o= (Oferente)nMalloc(sizeof(*o));
-	o->e=afuera;
-	o->precio = precio;
-	o->c = nMakeCondition(s->m);
-	return o;
-}
-
-double sumarPrecios(Subasta s){
-	double suma=0;
-	for (int i=0;i < s->count;i++){
-		suma= suma+s->o[i].p;
-	}
-	return suma;
-}
-
-// Programe aca las funciones nuevaSubasta, ofrecer y adjudicar
-
 Subasta nuevaSubasta(int unidades){
 	Subasta s = (Subasta)nMalloc(sizeof(*s));
 	s->m = nMakeMonitor();
@@ -84,13 +69,31 @@ Subasta nuevaSubasta(int unidades){
 	s->minIndex = 0;
 }
 
+int resultado(Oferente o){
+	// para cuando se llame a adjudicar:
+	// si el oferente está adentro de la subasta retorna TRUE
+	if (o->ready && o->e=adentro)
+		return TRUE;
+	// si esta afera retorna FALSE
+	else
+		return FALSE;
+}
+
+Oferente initOferta(Subasta s, double precio){
+	Oferente o= (Oferente)nMalloc(sizeof(*o));
+	o->e=afuera;
+	o->precio = precio;
+	o->c = nMakeCondition(s->c);
+	o->ready = FALSE;
+	return o;
+}
+
 int ofrecer(Subasta s, double precio){
 	// oferta (precio) de comprar un elemento de la subasta
 	Oferente O= initOferta(s,precio);
 	// esperando hasta que:
 	// 1. la subasta se cierre (se llame a adjudicar) retornando TRUE
 	// 2. los otros oferentes tienen mejores ofertas, retorna FALSE
-	nEnter(s->m);
 	if (s->count==0){ //primer oferente
 		O->e=dentro;
 		s->o[s->count]=O;
@@ -108,9 +111,8 @@ int ofrecer(Subasta s, double precio){
 		//comparar oferta con las ya existentes
 		int comp = compararPrecios(s,o);
 		// si tiene una oferta menor retorna False (2)
-		if (comp<=0){
+		if (comp<0){
 			O->e=afuera;
-			nExit(s->m);
 			return FALSE;
 		}
 		// si tiene una apuesta mayor expulsa al menor oferente y entra el (1)
@@ -121,7 +123,6 @@ int ofrecer(Subasta s, double precio){
 		}
 	}
 	nDestroyCondition(O->c);
-	nExit(s->m);
 	if( O->e == dentro){
 		return TRUE;
 	} else {
@@ -129,13 +130,19 @@ int ofrecer(Subasta s, double precio){
 	}
 }
 
+double sumarPrecios(Subasta s){
+	double suma=0;
+	for (int i=0;i < s->count;i++){
+		suma= suma+s->o[i].p;
+	}
+	return suma;
+}
+
 double adjudicar(Subasta s, int *punidades){
-	nEnter(s->m);
 	*punidades=s->n - s->count;
 	int suma=sumarPrecios(s);
 	for (int i=0;i < s->count;i++){
 		nSignalCondition(s->o[i]->c);//nNotifyAll penca
 	}
-	nExit(s->m);
 	return suma;
 }
