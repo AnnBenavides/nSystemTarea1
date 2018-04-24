9d5f4ad82631092ba80d3d1e65e7fba691230dcc
AnnBenavides <andrea.benavidesj@gmail.com>
2018-04-24 00:36:20 -0300

    fixing test2

--- subasta.c
+++ subasta.c
@@ -54,7 +54,7 @@ Subasta nuevaSubasta(int unidades){
 	s->m = nMakeMonitor();
 	s->n = unidades;
 	s->count = 0;
-	s->o = Oferente off[unidades];//probar sin *;
+	s->o = (Oferente*)nMalloc(unidades*sizeof(Oferente));//probar sin *
 	for(int i=0;i<unidades;i++){
 		s->o[i] = nMalloc(sizeof(Oferente));
 		nPrintf("\n\t\te->afuera, ");
